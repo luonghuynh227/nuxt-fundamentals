64d9d7c0aba06f4e0ed99671fce5ff0c0f7f47ad
huynh.nguyen <luonghuynh2525@gmail.com>
2020-04-13 17:26:30 +0700

    make it better

---
+++ components/TheNavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <!-- <a href="#" class="logo text-lg">
+        Nuxt Fundamentals
+      </a> -->
+      <nuxt-link class="logo text-lg" :to="{name: 'index'}">
+        Nuxt Fundamentals
+      </nuxt-link>
+      <div class="subheader">
+        A Vue School course
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ store/posts.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  all: [
+    {
+      id: 'balut',
+      title: 'What is Balut?',
+      content: 'you might think they were serving you a hardboiled egg. '
+    },
+    {
+      id: 'whereIsIt',
+      title: 'Where is the sign in button??',
+      content: 'I\'ve stalked the form for a while, reading everthing'
+    },
+    {
+      id: 'how',
+      title: 'How can I fix this problem?',
+      content: 'Please help me. I dont know what is the best way to fix my problem.'
+    }
+  ]
+})
+
